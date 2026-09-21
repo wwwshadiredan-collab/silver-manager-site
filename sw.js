b1845e9e84a5d6e5f321aaade89c693bc61e5752
wwwshadiredan-collab <www.shadiredan@gmail.com>
2026-09-21 14:43:03 -0900

    deploy: refresh cache for shop isolation

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'silver-manager-app-v21';
+const CACHE_NAME = 'silver-manager-app-v22';
 const APP_SHELL = [
   './',
   './index.html',
-  './assets/index-backupguard-20260922-1.js',
+  './assets/index-shopisolation-20260922-1.js',
   './assets/index-Glot_ggT.css',
   './manifest.webmanifest',
   './registerSW.js',
