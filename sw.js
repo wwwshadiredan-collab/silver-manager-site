96480ded5171c078892fae15eda9e6004e6cf951
wwwshadiredan-collab <www.shadiredan@gmail.com>
2026-09-21 13:27:35 -0900

    Refresh shell for stocktake details

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'silver-manager-app-v13';
+const CACHE_NAME = 'silver-manager-app-v14';
 const APP_SHELL = [
   './',
   './index.html',
-  './assets/index-stocktakeatomicfix-20260922-1.js',
+  './assets/index-stocktakedetails-20260922-1.js',
   './assets/index-Glot_ggT.css',
   './manifest.webmanifest',
   './registerSW.js',
