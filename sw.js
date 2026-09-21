93a2828bef698dc5e8afcb0038b449c34ed525e8
wwwshadiredan-collab <www.shadiredan@gmail.com>
2026-09-21 13:11:04 -0900

    Refresh shell for auto-sync race fix

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'silver-manager-app-v9';
+const CACHE_NAME = 'silver-manager-app-v10';
 const APP_SHELL = [
   './',
   './index.html',
-  './assets/index-democleanup-20260922-1.js',
+  './assets/index-autosyncracefix-20260922-1.js',
   './assets/index-Glot_ggT.css',
   './manifest.webmanifest',
   './registerSW.js',
